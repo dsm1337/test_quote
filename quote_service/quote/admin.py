8fc583daec0b3d80d5ea17e239956b95e32dfa69
Yakubov Nikita <yaknikitav@gmail.com>
2025-07-01 18:10:15 +0300

    models and admin added

---
+++ quote_service/quote/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from .models import Source, Quote, Opinion
+
+
+class QuoteInLine(admin.StackedInline):
+    model = Quote
+
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    inlines = (
+        QuoteInLine,
+    )
+    list_display = ('title',)
+
+
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = (
+        'author', 'source', 'text', 'views', 'weight'
+    )
+    search_fields = ('author', 'source', 'text')
+    list_editable = ('weight',)
+    list_filter = ('views', 'weight')
+    empty_value_display = 'Не задано'
+
+@admin.register(Opinion)
+class OpinionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'quote', 'created_at')
+    list_display_links = ('quote',)
---
+++ quote_service/quote/models.py
@@ -0,0 +1,67 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+
+User = get_user_model()
+
+
+class Source(models.Model):
+    '''Модель источников цитат'''
+    title = models.CharField('Название', max_length=100)
+
+    class Meta:
+        verbose_name = 'Источник'
+        verbose_name_plural = 'источники'
+
+    def __str__(self):
+        return title
+
+class Quote(models.Model):
+    '''Модель цитат'''
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    text = models.TextField('Текст')
+    source = models.ForeignKey(Source, on_delete=models.CASCADE, verbose_name='Источник')
+    views = models.IntegerField('Количество просмотров', default=0)
+    weight = models.IntegerField(
+        'Вес публикации', 
+        default=0,
+        validators=(
+            MaxValueValidator(100),
+            MinValueValidator(0)
+        )
+    )
+    created_at = models.DateTimeField('Время публикации', auto_now_add=True)
+
+    class Meta:
+        default_related_name = 'quotes'
+        ordering = ('weight','views', 'created_at')
+        verbose_name = 'Цитата'
+        verbose_name_plural = 'цитаты'
+
+    def get_likes(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return f'{self.source} - {self.text[:50]}'
+
+
+class Opinion(models.Model):
+    '''Модель лайков и дизлайков'''
+    OPINION_CHOICES = (
+        (-1, 'Dislike'),
+        (1, 'Like')
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quote = models.ForeignKey(Quote, on_delete=models.CASCADE)
+    value = models.SmallIntegerField('Мнение', choices=OPINION_CHOICES)
+    created_at = models.DateTimeField('Время проставления', auto_now_add=True)
+
+    class Meta:
+        default_related_name = 'likes'
+        ordering = ('created_at', )
+        verbose_name = 'Мнение'
+        verbose_name_plural = 'мнения'
+    
+    def __str__(self):
+        return f'{self.author}: {self.quote} реакция: {self.value}'
